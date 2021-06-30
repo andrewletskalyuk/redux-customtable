65157a3002aff72e9209a8b66b1cba8ba8be9519
ALetskalyuk <andrewletskalyuk@gmail.com>
2021-06-30 11:26:21 +0300

    first_30.06.2021

---
+++ src/actions.js
@@ -0,0 +1,21 @@
+export function loadData() {
+  return { type: "LOAD_DATA" };
+}
+
+export function changePageSize(size) {
+  return { type: "CHANGE_PAGE_SIZE", size };
+}
+
+export function addData() {
+  return { type: "ADD_DATA" };
+}
+
+export function updateData(row, value) {
+  const result = {
+    type: "UPDATE_DATA",
+    row,
+    value,
+  };
+  console.log("action updateData:", result);
+  return result;
+}
